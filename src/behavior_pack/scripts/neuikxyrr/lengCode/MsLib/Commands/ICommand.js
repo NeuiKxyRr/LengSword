c26b1f802322186172cf2dd040f79696209c8ef7
NeuiKxyRr <14749171+neuikxyrr@user.noreply.gitee.com>
2025-08-24 10:20:32 +0800

    Selcetion

--- src/behavior_pack/scripts/neuikxyrr/lengCode/MsLib/Commands/ICommand.js
+++ src/behavior_pack/scripts/neuikxyrr/lengCode/MsLib/Commands/ICommand.js
@@ -1,8 +1,12 @@
 import NodeList from "../Libs/NodeList";
+import Selection from "./Selection";
 
 class ISubCommand {
-    
+    constructor() {
+
+    }
 }
+
 class SubCommand extends NodeList {
     constructor() {
         super();
@@ -15,12 +19,19 @@ export default class ICommand {
      * @param {} selector 
      */
     constructor(name, selector, help, error, isOp) {
-        this._name_ = name;
-        this._help_ = help;
+        this._name_ = name;//主名称
+        this._help_ = help;//尽量将子命令的提示全部集成在这里
         this._error_ = error;
         this._isOp = isOp;
+        this.subCommands = new SubCommand();
     }
-    
 
+    addSubCommand(subCmd) {
+        this.subCommands.push(subCmd);
+    }
+
+    removeSubCommand() {
+        this.subCommands.clear();
+    }
 
 }
--- src/behavior_pack/scripts/neuikxyrr/lengCode/MsLib/Commands/Selection.js
+++ src/behavior_pack/scripts/neuikxyrr/lengCode/MsLib/Commands/Selection.js
@@ -1,6 +1,45 @@
+let SelectionTypes;
+(() => {
+    SelectionTypes["@a"] = "@a";
+    SelectionTypes["@p"] = "@p";
+    SelectionTypes["@r"] = "@r";
+    SelectionTypes["@e"] = "@e";
+    SelectionTypes["@s"] = "@s";
+})(SelectionTypes || (SelectionTypes = {}));
+
 class Selection {
-    constructor(type, designator) {
-        this.type = type;
-        this.designator = "@" + designator;
+
+    /**
+     * 获取选择器
+     * @param {string} type
+     * @returns {string}
+     */
+    get(type) {
+        if (!SelectionTypes[type]) throw new Error("no type of slection")
+        return type
+    }
+
+    /**
+     * 添加参数
+     * @param {string} slect 
+     * @param {object} params
+     */
+    addParameter(slect, params) {
+        slect.concat("[");
+        for (let p in params) {
+            slect.concat(`${p}=${params[p]},`);
+        }
+        slect.replace(/\,$/, "");
+        return slect.concat("]");
+    }
+
+    /**
+     * 清理参数
+     * @param {string} slect 
+     */
+    clearParameter(slect) {
+        return slect.replace(/  $$[^$$ ]*$$ /g, "");
     }
 }
+
+export default Selection;
